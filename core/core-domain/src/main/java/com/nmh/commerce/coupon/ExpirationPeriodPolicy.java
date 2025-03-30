842e2a7de65d7c64230f84b5cfcb17e5e007db7b
NaMinhyeok <nmh9097@gmail.com>
2025-03-30 21:55:26 +0900

    fix: DurationPeriodPolicy 만료 검증 로직 수정
    쿠폰 발급 시점을 기준으로 duration을 더한 시간 이후를 만료 시점으로 판단하도록 수정
    하지만 현재의 구조는 테스트하기 어려운 구조가 될 가능성이 높음

--- core/core-domain/src/main/java/com/nmh/commerce/coupon/ExpirationPeriodPolicy.java
+++ core/core-domain/src/main/java/com/nmh/commerce/coupon/ExpirationPeriodPolicy.java
@@ -3,5 +3,5 @@ package com.nmh.commerce.coupon;
 import java.time.LocalDateTime;
 
 public interface ExpirationPeriodPolicy {
-    void verify(LocalDateTime now);
+    void verify(LocalDateTime baseTime);
 }
