415aca31ef601ad312723e4b6277b37393d69dc1
NaMinhyeok <nmh9097@gmail.com>
2025-03-31 22:16:17 +0900

    feat: 쿠폰 재고 차감 구현

--- core/core-domain/src/main/java/com/nmh/commerce/coupon/CouponStock.java
+++ core/core-domain/src/main/java/com/nmh/commerce/coupon/CouponStock.java
@@ -16,4 +16,21 @@ public class CouponStock {
         this.remainingQuantity = remainingQuantity;
     }
 
+    public static CouponStock of(Long couponId, int remainingQuantity) {
+        return CouponStock.builder()
+            .couponId(couponId)
+            .remainingQuantity(remainingQuantity)
+            .build();
+    }
+
+    public CouponStock deductQuantity() {
+        if (!isRemaining()) {
+            throw new IllegalStateException("쿠폰 재고가 없습니다.");
+        }
+        return CouponStock.of(this.couponId, this.remainingQuantity - 1);
+    }
+
+    private boolean isRemaining() {
+        return remainingQuantity > 0;
+    }
 }
---
+++ core/core-domain/src/test/java/com/nmh/commerce/coupon/CouponStockManagerTest.java
@@ -0,0 +1,42 @@
+package com.nmh.commerce.coupon;
+
+import com.nmh.commerce.coupon.mock.FakeCouponStockRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class CouponStockManagerTest {
+
+    private CouponStockManager couponStockManager;
+
+    @BeforeEach
+    void setUp() {
+        FakeCouponStockRepository fakeCouponStockRepository = new FakeCouponStockRepository();
+        this.couponStockManager = CouponStockManager.builder()
+            .stockRepository(fakeCouponStockRepository)
+            .build();
+        CouponStock stock1 = CouponStock.builder()
+            .id(1L)
+            .couponId(1L)
+            .remainingQuantity(1)
+            .build();
+        CouponStock stock2 = CouponStock.builder()
+            .id(2L)
+            .couponId(2L)
+            .remainingQuantity(100)
+            .build();
+        fakeCouponStockRepository.save(stock1);
+        fakeCouponStockRepository.save(stock2);
+    }
+
+    @Test
+    void 쿠폰의_재고를_줄인다() {
+        // given
+        // when
+        CouponStock deductedCouponStock = couponStockManager.deductStock(1L);
+        // then
+        then(deductedCouponStock.getRemainingQuantity()).isEqualTo(0);
+    }
+
+}
