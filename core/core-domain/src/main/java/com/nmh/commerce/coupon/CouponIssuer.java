cee7dbfd7a17a89c94b609a630c905af079e19df
NaMinhyeok <nmh9097@gmail.com>
2025-03-31 22:17:35 +0900

    feat: 쿠폰 발급 시 재고 차감

--- core/core-domain/src/main/java/com/nmh/commerce/coupon/CouponIssuer.java
+++ core/core-domain/src/main/java/com/nmh/commerce/coupon/CouponIssuer.java
@@ -5,6 +5,7 @@ import com.nmh.commerce.user.UserRepository;
 import com.nmh.commerce.utils.LocalDateTimeHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Component
@@ -12,12 +13,15 @@ public class CouponIssuer {
     private final CouponRepository couponRepository;
     private final IssuedCouponRepository issuedCouponRepository;
     private final UserRepository userRepository;
+    private final CouponStockManager stockManager;
     private final LocalDateTimeHolder localDateTimeHolder;
 
+    @Transactional
     public IssuedCoupon issue(Long couponId, Long userId) {
         Coupon coupon = couponRepository.findById(couponId).orElseThrow(() -> new IllegalArgumentException("해당 쿠폰을 찾을 수 없습니다."));
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다."));
         IssuedCoupon issuedCoupon = IssuedCoupon.issue(coupon, user, localDateTimeHolder.now());
+        stockManager.deductStock(couponId);
         return issuedCouponRepository.save(issuedCoupon);
     }
 }
