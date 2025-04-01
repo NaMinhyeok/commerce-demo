2c504803be1817a302d2c43dad8a02fe14b2dd67
NaMinhyeok <nmh9097@gmail.com>
2025-04-01 13:57:44 +0900

    feat: 수량 vo로 사용하게 변경

---
+++ common/src/test/java/com/nmh/commerce/domain/QuantityTest.java
@@ -0,0 +1,62 @@
+package com.nmh.commerce.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.BDDAssertions.thenThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuantityTest {
+
+    @Test
+    void 수량은_0_이상이어야_한다() {
+        // given
+        // when
+        // then
+        thenThrownBy(() -> Quantity.of(-1))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("수량은 0 이상이어야 합니다.");
+    }
+
+    @Test
+    void 수량을_더한다() {
+        // given
+        Quantity quantity = Quantity.of(10);
+        // when
+        Quantity result = quantity.add(Quantity.of(5));
+        // then
+        then(result.getValue()).isEqualTo(15);
+    }
+
+    @Test
+    void 수량을_뺀다() {
+        // given
+        Quantity quantity = Quantity.of(10);
+        // when
+        Quantity result = quantity.subtract(Quantity.of(5));
+        // then
+        then(result.getValue()).isEqualTo(5);
+    }
+
+    @Test
+    void 재고가_0보다_작아지게_subtract_할_수_없다() {
+        // given
+        Quantity quantity = Quantity.of(10);
+        // when
+        // then
+        thenThrownBy(() -> quantity.subtract(Quantity.of(11)))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("수량이 부족합니다.");
+    }
+
+    @Test
+    void 재고가_남아있는지_여부를_확인한다() {
+        assertAll(
+            () -> then(Quantity.of(1).isRemaining()).isTrue(),
+            () -> then(Quantity.of(0).isRemaining()).isFalse()
+        );
+    }
+
+}
---
+++ infrastructure/db-core/src/main/java/com/nmh/commerce/utils/QuantityConverter.java
@@ -0,0 +1,25 @@
+package com.nmh.commerce.utils;
+
+import com.nmh.commerce.domain.Quantity;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class QuantityConverter implements AttributeConverter<Quantity, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(Quantity quantity) {
+        if (quantity == null) {
+            return null;
+        }
+        return quantity.getValue();
+    }
+
+    @Override
+    public Quantity convertToEntityAttribute(Integer integer) {
+        if (integer == null) {
+            return null;
+        }
+        return Quantity.of(integer);
+    }
+}
