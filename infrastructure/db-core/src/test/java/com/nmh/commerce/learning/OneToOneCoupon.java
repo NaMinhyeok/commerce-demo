e0a0b0dbc4d59536390e6b17c5ba0c7a768a7471
NaMinhyeok <nmh9097@gmail.com>
2025-04-04 23:54:46 +0900

    test: 테스트 환경 구성

---
+++ infrastructure/db-core/src/test/java/com/nmh/commerce/learning/OneToOneCoupon.java
@@ -0,0 +1,28 @@
+package com.nmh.commerce.learning;
+
+import jakarta.persistence.*;
+
+@Entity
+public class OneToOneCoupon {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "coupon", cascade = CascadeType.ALL)
+    private OneToOneCouponStock stock;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OneToOneCouponStock getStock() {
+        return stock;
+    }
+
+    public void setStock(OneToOneCouponStock stock) {
+        this.stock = stock;
+    }
+}
