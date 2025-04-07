3e2a44c4c82a2957fac853d5828350bdc7f4d61d
NaMinhyeok <nmh9097@gmail.com>
2025-04-07 14:43:13 +0900

    feat: ReentrantLock과 ConcurrentHashMap을 이용하여 동시성을 제어한다
    - Map이 존재하지 않으면 deductStock을 할 때 마다 Lock이 걸림
    - Map을 통해 ID 기반 제어를 통해 병렬처리

--- core/core-domain/src/main/java/com/nmh/commerce/coupon/CouponStockManager.java
+++ core/core-domain/src/main/java/com/nmh/commerce/coupon/CouponStockManager.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,10 +14,17 @@ import java.util.concurrent.locks.ReentrantLock;
 @Component
 public class CouponStockManager {
     private final CouponStockRepository stockRepository;
+    private final Map<Long, Lock> lockMap = new ConcurrentHashMap<>();
 
-    public synchronized CouponStock deductStock(Long couponId) {
-        CouponStock stock = stockRepository.findByCouponId(couponId)
-            .orElseThrow(() -> new IllegalArgumentException("해당 쿠폰 재고 정보를 찾을 수 없습니다."));
-        return stockRepository.save(stock.deductQuantity());
+    public CouponStock deductStock(Long couponId) {
+        Lock lock = lockMap.computeIfAbsent(couponId, id -> new ReentrantLock());
+        lock.lock();
+        try {
+            CouponStock stock = stockRepository.findByCouponId(couponId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 쿠폰 재고 정보를 찾을 수 없습니다."));
+            return stockRepository.save(stock.deductQuantity());
+        } finally {
+            lock.unlock();
+        }
     }
 }
