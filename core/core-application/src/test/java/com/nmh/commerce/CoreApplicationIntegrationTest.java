1626b8e84b543e5ab58fe554ae3d4deecf59b5ca
NaMinhyeok <nmh9097@gmail.com>
2025-04-16 16:10:51 +0900

    test: 어플리케이션 모듈 테스트환경 구성

---
+++ core/core-application/src/test/java/com/nmh/commerce/CoreApplicationIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.nmh.commerce;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
+
+@ActiveProfiles("local")
+@SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+public abstract class CoreApplicationIntegrationTest {
+}
