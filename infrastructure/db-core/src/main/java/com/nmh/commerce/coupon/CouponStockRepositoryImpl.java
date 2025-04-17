7a97f21c85b0b2b1e20a6987d4eb162fff25708d
NaMinhyeok <nmh9097@gmail.com>
2025-04-17 16:00:48 +0900

    feat: 낙관적 락 구현

--- infrastructure/db-core/src/main/java/com/nmh/commerce/coupon/CouponStockRepositoryImpl.java
+++ infrastructure/db-core/src/main/java/com/nmh/commerce/coupon/CouponStockRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.nmh.commerce.coupon;
 
+import jakarta.persistence.OptimisticLockException;
+import org.hibernate.StaleObjectStateException;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -22,6 +24,17 @@ public class CouponStockRepositoryImpl implements CouponStockRepository {
     public Optional<CouponStock> findByCouponId(Long couponId) {
         return couponStockJpaRepository.findByCouponId(couponId)
             .map(CouponStockEntity::toDomain);
+    }
+
+    @Override
+    public CouponStock update(CouponStock couponStock) {
+        try {
+            return couponStockJpaRepository.save(CouponStockEntity.from(couponStock)).toDomain();
+        } catch (OptimisticLockException | StaleObjectStateException e) {
+            throw new IllegalStateException("Failed to update coupon stock", e);
+        } catch (Exception e) {
+            throw new RuntimeException("An unexpected error occurred", e);
+        }
     }
 
 }
