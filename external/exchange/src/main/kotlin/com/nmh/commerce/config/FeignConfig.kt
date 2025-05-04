344ab066cec401dce6d90c168e9909bd84be4a7c
NaMinhyeok <nmh9097@gmail.com>
2025-05-04 16:36:29 +0900

    feat: exchange api feign을 통한 호출 요청 정의

--- external/exchange/src/main/kotlin/com/nmh/commerce/config/FeignConfig.kt
+++ external/exchange/src/main/kotlin/com/nmh/commerce/config/FeignConfig.kt
@@ -1,6 +1,15 @@
 package com.nmh.commerce.config
 
+import feign.RequestInterceptor
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @EnableFeignClients
-class FeignConfig
+class FeignConfig {
+
+    fun requestInterceptor(
+        @Value("\${exchange.host.access-key}") accessKey: String,
+    ): RequestInterceptor = RequestInterceptor { requestTemplate ->
+        requestTemplate.query("access_key", accessKey)
+    }
+}
