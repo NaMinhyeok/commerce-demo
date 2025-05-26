0f8852a3e56784b468176fb876549ec6cce9b1cc
NaMinhyeok <nmh9097@gmail.com>
2025-05-26 21:00:50 +0900

    feat: ehcache로 캐시 설정 및 구성 추가

---
+++ infrastructure/db-core/src/main/kotlin/com/nmh/commerce/config/CacheConfig.kt
@@ -0,0 +1,48 @@
+package com.nmh.commerce.config
+
+import com.nmh.commerce.cache.CacheSpec
+import org.ehcache.jsr107.EhcacheCachingProvider
+import org.slf4j.LoggerFactory
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.Duration
+import java.util.concurrent.TimeUnit
+import javax.cache.CacheManager
+import javax.cache.Caching
+import javax.cache.configuration.MutableConfiguration
+import javax.cache.expiry.CreatedExpiryPolicy
+import javax.cache.expiry.Duration as JCacheDuration
+
+@Configuration
+@EnableCaching
+class CacheConfig {
+    private val logger = LoggerFactory.getLogger(CacheConfig::class.java)
+
+    @Bean
+    fun ehCacheConfig(): CacheManager {
+        val provider = Caching.getCachingProvider() as EhcacheCachingProvider
+        val cacheManager = provider.cacheManager
+
+        CacheSpec.entries.forEach { spec ->
+            val config = createCacheConfig(spec.cacheName, spec.ttl)
+            if (cacheManager.cacheNames.contains(spec.cacheName)) {
+                logger.warn("캐시가 이미 존재합니다: ${spec.cacheName}, 기존 캐시를 재사용합니다.")
+                return@forEach
+            }
+            cacheManager.createCache(spec.cacheName, config)
+        }
+
+        logger.info("캐시 설정이 초기화되었습니다: ${cacheManager.cacheNames}")
+
+        return cacheManager
+    }
+
+    private fun createCacheConfig(cacheName: String, ttl: Duration) = MutableConfiguration<String, Any>().apply {
+        setTypes(String::class.java, Any::class.java)
+        setStoreByValue(false)
+        setStatisticsEnabled(true)
+        setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(JCacheDuration(TimeUnit.SECONDS, ttl.seconds)))
+        logger.info("캐시 설정 완료 캐시이름 : $cacheName with TTL: $ttl")
+    }
+}
---
+++ infrastructure/db-core/src/test/kotlin/com/nmh/commerce/config/CacheConfigTest.kt
@@ -0,0 +1,20 @@
+package com.nmh.commerce.config
+
+import com.nmh.commerce.CoreDbContextTest
+import com.nmh.commerce.cache.CacheSpec
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Test
+import javax.cache.CacheManager
+
+class CacheConfigTest(
+    private val cacheManager: CacheManager,
+) : CoreDbContextTest() {
+
+    @Test
+    fun `모든 CacheSpec의 캐시는 등록되어야 한다`() {
+        val registeredCaches = cacheManager.cacheNames.toSet()
+        val expectedCaches = CacheSpec.entries.map { it.cacheName }.toSet()
+
+        then(registeredCaches).isEqualTo(expectedCaches)
+    }
+}
