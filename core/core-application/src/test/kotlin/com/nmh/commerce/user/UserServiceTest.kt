3b6189c17479f5bdc509e9384496494bc26ac62d
NaMinhyeok <nmh9097@gmail.com>
2025-05-28 20:10:42 +0900

    feat: caffeine cache 적용
    - ehcache는 cache가 사전에 정의되어있지 않으면 사용할 수 없었으나 caffeine은 사용 가능
    - caffeine은 springframework.cache를 사용할 수 있으나 ehcache는 jcache를 사용해야함

--- core/core-application/src/test/kotlin/com/nmh/commerce/user/UserServiceTest.kt
+++ core/core-application/src/test/kotlin/com/nmh/commerce/user/UserServiceTest.kt
@@ -2,6 +2,8 @@ package com.nmh.commerce.user
 
 import com.nmh.commerce.CoreApplicationIntegrationTest
 import org.assertj.core.api.BDDAssertions.then
+import org.assertj.core.api.BDDAssertions.thenThrownBy
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.times
 import org.springframework.transaction.annotation.Transactional
@@ -23,5 +25,19 @@ class UserServiceTest(
 
         // Then
         then(result).isEqualTo(savedUser)
+    }
+
+    @Disabled("ehcache의 경우는 등록되어있지 않은 경우는 예외를 발생시킨다. 따라서 ehcache를 사용하지 않는 경우 이 테스트는 비활성화한다.")
+    @Test
+    fun `캐시가 등록되어있지 않으면 캐시를 찾을 수 없는 예외가 발생한다`() {
+        // given
+        val user = User(id = 0, name = "Test User")
+        val savedUser = userRepository.save(user)
+
+        // when
+        // then
+        thenThrownBy { userService.getUserWithDefaultCacheName(savedUser.id) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Cannot find cache named 'ANYCACHENAME'")
     }
 }
--- infrastructure/cache/build.gradle.kts
+++ infrastructure/cache/build.gradle.kts
@@ -4,4 +4,7 @@ dependencies {
     implementation("org.ehcache:ehcache:3.10.8")
     implementation("javax.cache:cache-api:1.1.1")
     implementation("org.springframework.boot:spring-boot-starter-cache")
+
+    // Caffeine Cache
+    implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
 }
